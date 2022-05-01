d1cd9bab5f5f5bc10f67eee4f20e3cb20313064e
Camila Storck <mcamilastorck@gmail.com>
2022-05-01 19:17:43 -0300

    Initial Commit

---
+++ BMICalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMICalculator
+//
+//  Created by Apple  on 01/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
